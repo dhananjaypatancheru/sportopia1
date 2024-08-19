488a803a67d3d5d0ab72b26b40c213d2208766fe
dhananjay <dhananjaybittu7@gmail.com>
2024-08-19 12:51:03 +0100

    product detail page completed

--- app/src/main/java/com/dhanunjay/sportopia/SportopiaProductDetails.kt
+++ app/src/main/java/com/dhanunjay/sportopia/SportopiaProductDetails.kt
@@ -1,20 +1,34 @@
 package com.dhanunjay.sportopia
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.dhanunjay.sportopia.ui.Theme.SportopiaTheme
 
 class DetailsPageActivity : ComponentActivity() {
@@ -35,7 +49,6 @@ class DetailsPageActivity : ComponentActivity() {
     }
 
 
-
 }
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -47,6 +60,110 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     ) {
 
         //details
+
+        Box(
+            modifier = Modifier
+                .align(alignment = Alignment.TopStart)
+                .offset(x = (-7).dp,
+                    y = (-86).dp)
+                .requiredWidth(width = 375.dp)
+                .requiredHeight(height = 812.dp)
+                .background(color = Color.White)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.cm),
+                contentDescription = "",
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 0.dp,
+                        y = 160.dp)
+                    .requiredWidth(width = 375.dp)
+                    .requiredHeight(height = 463.dp))
+
+
+            Text(
+                text = "Cricket Kit ",
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 20.dp,
+                        y = 640.dp))
+
+            Text(
+                text = "Discover the ultimate cricket kit for all levels! \n" +
+                        "Premium quality gear including bats, balls, pads, \n" +
+                        "gloves, and helmets. Elevate your game and \n" +
+                        "dominate the field. Gear up now!.",
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Medium),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 20.dp,
+                        y = 675.dp))
+
+
+
+            val localContext = LocalContext.current
+            Box(
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 0.dp,
+                        y = 80.dp)
+                    .requiredWidth(width = 440.dp)
+                    .requiredHeight(height = 60.dp)
+                    .clip(shape = RoundedCornerShape(15.dp))
+                    .background(color = Color(0xFFF44336))
+                    .clickable {
+                        localContext.startActivity(
+                            Intent(localContext, AddtoCartActivity::class.java)
+                        )
+                    }
+            )
+            Text(
+                text = "Details Page",
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 120.dp,
+                        y = 100.dp))
+            Box(
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 82.dp,
+                        y = 760.dp)
+                    .requiredWidth(width = 240.dp)
+                    .requiredHeight(height = 50.dp)
+                    .clip(shape = RoundedCornerShape(15.dp))
+                    .background(color = Color(0xFFF44336))
+                    .clickable {
+                        localContext.startActivity(
+                            Intent(localContext, AddtoCartActivity::class.java)
+                        )
+                    }
+            )
+            Text(
+                text = "Add to Cart",
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 160.dp,
+                        y = 775.dp))
+
+
+        }
+
+
     }
 }
 
