201919df28aec2d430f081ff866be19ee75ff371
dhananjay <dhananjaybittu7@gmail.com>
2024-08-19 12:58:10 +0100

    cart page completed

--- app/src/main/java/com/dhanunjay/sportopia/SportopiaCartList.kt
+++ app/src/main/java/com/dhanunjay/sportopia/SportopiaCartList.kt
@@ -1,15 +1,42 @@
 package com.dhanunjay.sportopia
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+
+
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Slider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 import com.dhanunjay.sportopia.ui.Theme.SportopiaTheme
 
 class AddtoCartActivity : ComponentActivity() {
@@ -35,10 +62,131 @@ class AddtoCartActivity : ComponentActivity() {
 
 @Composable
 fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .requiredWidth(width = 360.dp)
+            .requiredHeight(height = 640.dp)
+            .background(color = Color.White)
+    ) {
+        Box(
+            modifier = Modifier
+                .align(alignment = Alignment.TopStart)
+                .offset(x = (-7).dp,
+                    y = (-86).dp)
+                .requiredWidth(width = 375.dp)
+                .requiredHeight(height = 812.dp)
+                .background(color = Color.White)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.arrow_left),
+                contentDescription = "arrow-left",
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 27.dp,
+                        y = 35.dp)
+                    .requiredSize(size = 24.dp))
+
+            var sliderPosition by remember { mutableStateOf(0f) }
+            Slider(
+                value = sliderPosition,
+                onValueChange = { sliderPosition = it },
+                onValueChangeFinished = { },
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 316.dp,
+                        y = 27.dp)
+                    .requiredSize(size = 32.dp))
 
 
+            val context = LocalContext.current
+            Box(
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 27.dp,
+                        y = 738.dp)
+                    .requiredWidth(width = 321.dp)
+                    .requiredHeight(height = 50.dp)
+                    .clickable {
+                        context.startActivity(
+                            Intent(context, PaymentActivity::class.java))
+                    }
 
-    //
+                    .clip(shape = RoundedCornerShape(15.dp))
+                    .background(color = Color(0xFFF44336)))
+            Text(
+                text = "Proceed to Checkout",
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 108.dp,
+                        y = 753.dp))
+            Box(
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 0.dp,
+                        y = 80.dp)
+                    .requiredWidth(width = 431.dp)
+                    .requiredHeight(height = 70.dp)
+                    .clip(shape = RoundedCornerShape(15.dp))
+                    .background(color = Color(0xFFF44336)))
+            Text(
+                text = " Shopping Cart ",
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 115.dp,
+                        y = 110.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.pic),
+                contentDescription = "",
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 7.dp,
+                        y = 410.dp)
+                    .requiredWidth(width = 380.dp)
+                    .requiredHeight(height = 140.dp)
+                    .clip(shape = RoundedCornerShape(15.dp)))
+            Image(
+                painter = painterResource(id = R.drawable.pic2),
+                contentDescription = "",
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 7.dp,
+                        y = 255.dp)
+                    .requiredWidth(width = 380.dp)
+                    .requiredHeight(height = 140.dp)
+                    .clip(shape = RoundedCornerShape(24.dp)))
+            Image(
+                painter = painterResource(id = R.drawable.total),
+                contentDescription = "",
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 7.dp,
+                        y = 560.dp)
+                    .requiredWidth(width = 380.dp)
+                    .requiredHeight(height = 120.dp)
+                    .clip(shape = RoundedCornerShape(24.dp)))
+            Text(
+                text = "Your Cart Items",
+                color = Color.Black,
+                style = TextStyle(
+                    fontSize = 26.sp,
+                    fontWeight = FontWeight.Medium),
+                modifier = Modifier
+                    .align(alignment = Alignment.TopStart)
+                    .offset(x = 110.dp,
+                        y = 180.dp))
+
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
