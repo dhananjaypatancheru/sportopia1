2f67b5afa27ed2ed88bdd42bb3bf15c45febf6a3
dhananjay <dhananjaybittu7@gmail.com>
2024-08-20 06:50:12 +0100

    ordesuccesspage

--- app/src/main/java/com/dhanunjay/sportopia/ThankYou.kt
+++ app/src/main/java/com/dhanunjay/sportopia/ThankYou.kt
@@ -1,11 +1,121 @@
 package com.dhanunjay.sportopia
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.dhanunjay.sportopia.ui.Theme.Primary
 
 
 class ThankYou : ComponentActivity() {
+    @SuppressLint("SuspiciousIndentation")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            val context = LocalContext.current
+            val gradient45 = Brush.linearGradient(
+                colors = listOf(Color.White, Color.White),
+                start = Offset(0f, Float.POSITIVE_INFINITY),
+                end = Offset(Float.POSITIVE_INFINITY, 0f)
+            )
+
+            Surface(
+                modifier = Modifier
+                    .background(gradient45)
+                    .fillMaxSize()
+                    .padding(28.dp)
+
+                    .background(Color.White)
+                    .padding(28.dp)
+                //.align(Alignment.Center)
+            ) {
 
 
+                Column(modifier = Modifier
+                    .fillMaxSize()
+                    .background(gradient45),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally) {
+                    HeadingTextComponent(value = "Order Placed Successfully")
+                    Spacer(modifier = Modifier.height(50.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.os),
+                        contentDescription = "",
+                        modifier = Modifier
+                            .offset(x = 0.dp,
+                                y = 10.dp)
+                            .requiredWidth(width = 170.dp)
+                            .requiredHeight(height = 170.dp))
+                    androidx.compose.material3.Text(
+                        text = "Thank you for placing your order! \n" +
 
+                                "We are glad to serve you on your journey.",
+                        color = Color.Black,
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Medium
+                        ),
+                        modifier = Modifier
+
+                            .offset(
+                                x = 20.dp,
+                                y = 40.dp
+                            )
+                    )
+
+                    Spacer(modifier = Modifier.height(80.dp))
+                    val sports = ""
+                    Button(
+                        modifier = Modifier
+                            .wrapContentWidth()
+                            .heightIn(48.dp),
+                        onClick = {
+                            context.startActivity(Intent(context, MainActivity::class.java)
+                                .putExtra("sportopia", sports))
+                        },
+                        contentPadding = PaddingValues(),
+                        colors = ButtonDefaults.buttonColors(Color.Transparent),
+                        shape = RoundedCornerShape(50.dp),
+                    ) {
+
+                    }
+                }
+            }
+        }
+    }
 }
 
